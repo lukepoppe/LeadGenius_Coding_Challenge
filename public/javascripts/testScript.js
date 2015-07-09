aaba3f4ded3d9cbff4ba852672520c8a264df246
lukepoppe <lukepoppe@hotmail.com>
2015-07-09 14:08:22 -0500

    pulled first page

--- public/javascripts/testScript.js
+++ public/javascripts/testScript.js
@@ -1,1 +1,4 @@
 //console.log("testScript.js Loading");
+$( document ).ready(function() {
+   console.log(eventTitle);
+});
--- routes/server.js
+++ routes/server.js
@@ -4,44 +4,40 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
 
+app.get('/scrape', function(req, res){
+    var eventList =[];
     url = 'http://events.stanford.edu/2014/October/1/';
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
-
-            var title, date, time;
-            var json = { title : "", date : "", time : ""};
-
-            $('.postcard-text').filter(function(){
-                var data = $(this);
-                title = data.children().first().text();
-                date = data.children().last().children().text();
-                time = data.children().last().children().children().text();
-
-                json.title = title;
-                json.date = date;
-                json.time = time;
-            })
 
 
-        }
+        request(url, function(error, response, html){
+            if(!error){
+                var $ = cheerio.load(html);
+                //var title, eventInfo;
 
+                    $('.postcard-text').each(function(){
+                        var json = { title : "", eventInfo : ""};
 
+                        var title = $(this).find('h3').text();
+                        json.title = title;
+                        console.log(title);
+                        eventList.push(json);
 
-        fs.writeFile('eventsData.json', JSON.stringify(json, null, 4), function(err){
+                        var eventInfo =  $(this).find('strong').text();
+                        json.eventInfo = eventInfo;
+                        console.log(eventInfo);
+                        eventList.push(json);
+                });
+            }
+            fs.writeFile('eventsData.json', JSON.stringify(eventList, null, 4), function(err){
+                console.log('File successfully written! - Check your project directory for the eventsData.json file');
+            });
+    //app does not have a UI.
+            res.send('Check console!')
+        }) ;
+});
 
-            console.log('File successfully written! - Check your project directory for the eventsData.json file');
-
-        })
-
-//app does not have a UI.
-        res.send('Check console!')
-
-    }) ;
-})
-app.listen('8090')
+app.listen('8090');
 console.log('Magic happens on port 8090');
 exports = module.exports = app;
